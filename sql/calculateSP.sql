664ad7dc57690a865192930afefcf5980426ff82
jamesquinlan <jquinlan@une.edu>
2023-01-09 09:33:42 -0500

    Stored Procedure: Calculate Sum and Product

---
+++ sql/calculateSP.sql
@@ -0,0 +1,20 @@
+-- +++++++++++++++++++++++++++++++++
+-- Author: 		James Quinlan
+-- Date: 		Apr 09, 2021
+-- Purpose:		SQL PROCEDURE
+-- Copyright:	(c) 2021. J. Quinlan
+-- License: 	MIT 
+
+-- MySQL Version: 8.0.20
+-- +++++++++++++++++++++++++++++++++
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS calculate //
+CREATE PROCEDURE calculate(IN x INT, IN y INT, OUT sum INT, OUT product INT)
+-- Calculate Sum and Product of two numbers
+BEGIN
+  SET sum = x + y;
+  SET product = x * y;
+END//
+
+DELIMITER ;
