2c6ec8da0f615c80ab036cba1508333feecab5aa
jamesquinlan <jquinlan@une.edu>
2023-03-24 07:21:23 -0400

    nycflights database

---
+++ data/nycflights/airlines.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Airlines(
+   carrier VARCHAR(2) NOT NULL PRIMARY KEY
+  ,name    VARCHAR(24) NOT NULL
+);
+INSERT INTO Airlines(carrier,name) VALUES ('AA','American Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('AS','Alaska Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('B6','JetBlue Airways');
+INSERT INTO Airlines(carrier,name) VALUES ('DL','Delta Air Lines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('EV','ExpressJet Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('F9','Frontier Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('HA','Hawaiian Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('NK','Spirit Air Lines');
+INSERT INTO Airlines(carrier,name) VALUES ('OO','SkyWest Airlines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('UA','United Air Lines Inc.');
+INSERT INTO Airlines(carrier,name) VALUES ('VX','Virgin America');
+INSERT INTO Airlines(carrier,name) VALUES ('WN','Southwest Airlines Co.');
