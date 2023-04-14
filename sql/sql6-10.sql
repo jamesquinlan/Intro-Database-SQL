16a430d26e953b9e27329cf3586ef30144af0f07
James Quinlan <jquinlan@une.edu>
2023-04-13 21:40:43 -0400

    Update sql6-10.sql

--- sql/sql6-10.sql
+++ sql/sql6-10.sql
@@ -1,11 +1,11 @@
--- ****************************************************
--- Description:	How many seats were on the FIRST plane 
---              leaving JFK to ORD on 12/25? 
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Question:
+-- How many seats were on the FIRST plane leaving JFK to ORD on 12/25?  
 -- 
 -- Course: 		Intro. to Database
 -- Author: 		Quinlan, J.
 -- Date:		2023-04-13
--- ****************************************************
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 use nycFlights;
 SELECT 
