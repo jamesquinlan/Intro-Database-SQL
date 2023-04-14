bad140d84478f34408414a9c59829710d2f1939e
James Quinlan <jquinlan@une.edu>
2023-04-13 21:45:13 -0400

    Update sql6-8.sql

--- sql/sql6-8.sql
+++ sql/sql6-8.sql
@@ -1,11 +1,11 @@
--- ****************************************************
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- Description:	What was the flight number of the first 
 --              flight on Christmas to Chicago from JFK?
 -- 
 -- Course: 		Intro. to Database
 -- Author: 		Quinlan, J.
 -- Date:		2023-04-13
--- ****************************************************
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 use nycFlights;
 SELECT 
